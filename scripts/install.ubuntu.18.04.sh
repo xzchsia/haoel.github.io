21fbfaa7ede54b2b9e1a37d6e052a8825a522bf4
xzchsia <xzchsia@outlook.com>
2025-01-06 17:32:00 +0800

    bbr更新： 这一行代码的作用是将 bbr 添加到系统中可用的 TCP拥塞控制算法列表中。具体来说，这个命令将 bbr 添加到 /etc/sysctl.conf 配置文件中，使其成为系统的一个可用选项。这可以确保在任何时候都可以选择使用 bbr 作为 TCP 拥塞控制算法。
    为什么新增这一行代码：
    确保可用性：通过将 bbr 添加到可用的 TCP拥塞控制算法列表中，可以确保无论何时都可以选择并启用 bbr。
    提高兼容性：某些系统配置可能需要明确声明 bbr 是一个可用的选项，以避免潜在的问题。
    
    Signed-off-by: xzchsia <xzchsia@outlook.com>

--- scripts/install.ubuntu.18.04.sh
+++ scripts/install.ubuntu.18.04.sh
@@ -36,6 +36,7 @@ start_bbr(){
     echo "tcp_bbr" | sudo tee --append /etc/modules-load.d/modules.conf
     echo "net.core.default_qdisc=fq" | sudo tee --append /etc/sysctl.conf
     echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee --append /etc/sysctl.conf
+    echo "net.ipv4.tcp_available_congestion_control=bbr" | sudo tee --append /etc/sysctl.conf
     sudo sysctl -p
     sysctl net.ipv4.tcp_available_congestion_control
     sysctl net.ipv4.tcp_congestion_control
