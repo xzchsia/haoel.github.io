50825eba8c2c7e095a3a0908b1fc05a1de98fa1d
xzchsia <xzchsia@outlook.com>
2025-11-07 09:42:32 +0800

    install命令添加-y参数

--- scripts/install.ubuntu.18.04.sh
+++ scripts/install.ubuntu.18.04.sh
@@ -65,10 +65,9 @@ install_docker() {
     if ! command -v docker &> /dev/null; then
         echo "开始安装 Docker CE"
 
-
         # Add Docker's official GPG key:
         sudo apt-get update -qq
-        sudo apt-get install ca-certificates curl
+        sudo apt-get install -y ca-certificates curl
         sudo install -m 0755 -d /etc/apt/keyrings
         sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
         sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -80,7 +79,7 @@ install_docker() {
         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         sudo apt-get update -qq
 
-        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
         # 检查 Docker 是否正常运行
         sudo systemctl status docker
